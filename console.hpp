232a3fdcc0ae1e7ebd2d3c78a61472d447d07d37
A042XD <AlexYJHP@Outlook.com>
2024-06-28 08:57:57 -0400

    Separated Functions
    Separated functions related to the console into a header.

---
+++ console.hpp
@@ -0,0 +1,38 @@
+#include<windows.h>
+#include<stdio.h>
+#include<algorithm>
+#include<deque>
+#include<ctime>
+
+#define pii pair<int,int>
+#define mp make_pair
+#define f first
+#define s second
+
+void clear();
+bool keyDown(int k);
+void moveCursor(int x,int y);
+void fill(int x1,int y1,int x2,int y2,char c);
+void text(int x,int y,char s[]);
+
+void clear(){
+  system("cls");
+}
+bool keyDown(int k){
+  return GetAsyncKeyState(k)&0x8000?1:0;
+}
+void moveCursor(int x,int y){
+  HANDLE hOut=GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD pos;
+  pos.X=x,pos.Y=y;
+  SetConsoleCursorPosition(hOut,pos);
+}
+void fill(int x1,int y1,int x2,int y2,char c){
+  for(int i=x1;i<=x2;i++)
+    for(int j=y1;j<=y2;j++)
+      moveCursor(i,j),printf("%c",c);
+}
+void text(int x,int y,char s[]){
+  moveCursor(x,y);
+  printf("%s",s);
+}
--- main.cpp
+++ main.cpp
@@ -9,23 +9,9 @@
 
  */
 
-#include<windows.h>
-#include<stdio.h>
-#include<algorithm>
-#include<deque>
-#include<ctime>
+#include"console.hpp"
 
 using namespace std;
-
-// Functions for drawing on the console
-#define pii pair<int,int>
-#define mp make_pair
-#define f first
-#define s second
-void clear();
-bool keyDown(int k);
-void moveCursor(int x,int y);
-void fill(int x1,int y1,int x2,int y2,char c);
 
 // Variables & Functions for the game
 deque<pii> sp;
@@ -49,12 +35,10 @@ int main(){
   clear();
 
   // Setup
-  moveCursor(2,2);
-  puts("Snake Game");
+  text(2,2,"Snake Game");
   fill(2,4,16,18,'.');
   add(9,11);
-  moveCursor(9,11);
-  puts("%");
+  text(9,11,"%");
   createFood();
   drawFood();
   
@@ -73,52 +57,29 @@ int main(){
     last_time=(double)clock()/CLOCKS_PER_SEC;
 
     // Updating the snake's position
-    moveCursor(sp.front().f,sp.front().s);
-    puts("#");
+    text(sp.front().f,sp.front().s,"#");
     int x=sp.front().f+dx[dir];
     int y=sp.front().s+dy[dir];
     sp.push_front(mp(x,y));
-    moveCursor(x,y);
-    puts("%");
+    text(x,y,"%");
 
     // Game Over
-    if(x<2||x>16)break;
-    if(y<4||y>18)break;
+    if(x<2||x>16||y<4||y>18)break;
 
     // Eat Food
     if(x==fdx+1&&y==fdy+3){
-      fdx=rand()%15+1,fdy=rand()%15+1;
+      fdx=rand()%15+1;
+      fdy=rand()%15+1;
+      continue;
     }
-    else{
-      moveCursor(sp.back().f,sp.back().s);
-      puts(".");
-      sp.pop_back();
-    }
+    text(sp.back().f,sp.back().s,".");
+    sp.pop_back();
     drawFood();
   }
 
   // Game Over
-  moveCursor(2,2);
-  puts("Game Over!");
+  text(2,2,"Game Over ");
   moveCursor(1,20);
   system("PAUSE");
   return 0;
-}
-
-void moveCursor(int x,int y){
-  HANDLE hOut=GetStdHandle(STD_OUTPUT_HANDLE);
-  COORD pos;
-  pos.X=x,pos.Y=y;
-  SetConsoleCursorPosition(hOut,pos);
-}
-void clear(){
-  system("cls");
-}
-bool keyDown(int k){
-  return GetAsyncKeyState(k)&0x8000?1:0;
-}
-void fill(int x1,int y1,int x2,int y2,char c){
-  for(int i=x1;i<=x2;i++)
-    for(int j=y1;j<=y2;j++)
-      moveCursor(i,j),printf("%c",c);
 }
